4fcbffc239bc6eabe70f77082bad7828a9599715
OSHMOS <ojames9707@gmail.com>
2021-12-21 22:51:46 +0900

    계산기 프로젝트 형태 구현 및 함수 실행 확인

---
+++ practice_JavaScript/JavaScript Practice Calculator/index.js
@@ -0,0 +1,16 @@
+let box = document.getElementsByClassName(calculator_box);
+
+let num9 = document.getElementsByClassName(num9).value;
+let num8 = document.getElementsByClassName(num8).value;
+let num7 = document.getElementsByClassName(num7).value;
+let num6 = document.getElementsByClassName(num6).value;
+let num5 = document.getElementsByClassName(num5).value;
+let num4 = document.getElementsByClassName(num4).value;
+let num3 = document.getElementsByClassName(num3).value;
+let num2 = document.getElementsByClassName(num2).value;
+let num1 = document.getElementsByClassName(num1).value;
+
+function insertNumber(a){
+    console.log(a);
+};
+
