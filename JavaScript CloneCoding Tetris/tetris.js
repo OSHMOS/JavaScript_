9ca8061bf143631c79361e7bc5b3b1d7d27db00c
OSHMOS <ojames9707@gmail.com>
2022-02-20 18:22:03 +0900

    자바스크립트 클론코딩 : 테트리스 완성, 실행이 안돼서 오류 찾는 중

--- JavaScript CloneCoding Tetris/tetris.js
+++ JavaScript CloneCoding Tetris/tetris.js
@@ -1,5 +1,10 @@
+import BLOCKS from "./blocks.js";
+
 // DOM
 const playground = document.querySelector(".playground > ul");
+const gameText = document.querySelector(".game-text");
+const scoreDisplay = document.querySelector(".score");
+const restartButton = document.querySelector(".game-text > button");
 
 // Setting
 const GAME_ROWS = 20;
@@ -11,49 +16,166 @@ let duration = 500;
 let downInterval;
 let tempMovingItem;
 
-const BLOCKS = {
-    tree: [
-        [[2,1], [0,1], [1,1], [1,1]],
-        [],
-        [],
-        [],
-    ]
-}
 const movingItem = {
-    type: "tree",
-    direction: 0,
+    type: "",
+    direction: 3,
     top: 0,
-    left: 3,
+    left: 0,
 };
+
+init()
 
 // functions
 function init(){
-    tempMovingItem = { ...movingItem };
-
+    tempMovingItem = {...movingItem};
     for (let i = 0; i < GAME_ROWS; i++) {
         prependNewLine()
     }
-    renderBlocks()
+    generateNewBlock()
 }
 
 function prependNewLine() {
-        const li = document.createElement("li");
-        const ul = document.createElement("ul");
-        for (let j = 0; j < GAME_COLS; j++) {
-            const matrix = document.createElement("li");
-            ul.prepend(matrix);
-        }
-        li.prepend(ul);
-        playground.prepend(li);
+    const li = document.createElement("li");
+    const ul = document.createElement("ul");
+    for (let j = 0; j < GAME_COLS; j++) {
+        const matrix = document.createElement("li");
+        ul.prepend(matrix);
+    }
+    li.prepend(ul);
+    playground.prepend(li);
 }
 
-function renderBlocks() {
+function renderBlocks(moveType = "") {
     const { type, direction, top, left } = tempMovingItem;
-
-    BLOCKS[type][direction].forEach(block=>{
+    const movingBlocks = document.querySelectorAll(".moving");
+    movingBlocks.forEach(moving => {
+        moving.classList.remove(type, "moving");
+    })
+    BLOCKS[type][direction].some(block=>{
         const x = block[0] + left;
-        const y = block[1];
-        const target = playground.childNodes[y].childNodes[0].childNodes[x];
-        target.classList.add(type);
+        const y = block[1] + top;
+        const target = playground.childNodes[y] ? playground.ChildNodes[y].childNodes[0].childNodes[x] : null;
+        const isAvailable = checkEmpty(target);
+        if (isAvailable) {
+            target.classList.add(type, "moving")
+        } else {
+            tempMovingItem = { ...movingitem }
+            if (moveType === "retry") {
+                clearInterval(downInterval);
+                showGameoverText()
+            }
+            setTimeout(() => {
+                renderBlocks("retry");
+                if(moveType === "top"){
+                    seizeBlock();
+                }
+            }, 0)
+            return true;
+        }
     })
 }
+
+function checkEmpty(target) {
+    if(!target || target.classList.contations("seized")) {
+        return false;
+    }
+    return true;
+}
+
+function moveBlock(moveType, amount) {
+    tempMovingItem[moveType] += amount
+    renderBlocks(moveType)
+}
+
+function seizeBlock() {
+    const movingBlocks = document.querySelectorAll(".moving");
+    movingBlocks.forEach(moving => {
+        moving.classList.remove("moving");
+        moving.classList.add("seized");
+    })
+    checkMatch()
+}
+
+function checkMatch() {
+
+    const childNodes = playground.childNodes;
+    childNodes.forEach(child=>{
+        let matched = true;
+        child.children[0].childNodes.forEach(li=>{
+            if (!li.classList.contains("seized")) {
+                matched = false;
+            }
+        })
+        if (matched) {
+            child.remove();
+            prependNewLine()
+            score++;
+            scoreDisplay.innerHTML = score;
+        }
+    })
+
+    generateNewBlock()
+}
+function generateNewBlock() {
+
+    clearInterval(downInterval);
+    downInterval = setInterval(() => {
+        moveBlock("top", 1)
+    },duration)
+    const blockArray = Object.entries(BLOCKS);
+    const randomIndex = Math.floor(Math.random() * blockArray.length);
+
+    movingItem.type = "blockArray[randomIndex][0]"
+    movingItem.top = 0;
+    movingItem.left = 3;
+    movingItem.direction = 0;
+    tempMovingItem = {...movingItem};
+    renderBlocks()
+}
+
+function changeDirection() {
+    const direction = tempMovingItem.direction;
+    direction === 3 ? tempMovingItem.direction = 0 : tempMovingItem.direction += 1;
+    renderBlocks()
+}
+
+function dropBlock() {
+    clearInterval(downInterval);
+    downInterval = setInterval(() => {
+         moveBlock("top", 1)
+    },10)
+}
+
+function showGameoverText() {
+    gameText.style
+}
+
+// event handling
+document.addEventListener("keydown", e => {
+    switch(e.keyCode){
+        case 39:
+            moveBlock("left", 1);
+            break;
+        case 37:
+            moveBlock("left", -1);
+            break;
+        case 38:
+            changeDirection();
+            break;
+        case 40:
+            moveBlock("top", 1);
+            break;
+        case 32:
+            dropBlock();
+            break;
+        default:
+            break;
+    }
+    console.log(e);
+})
+
+restartButton.addEventListener("click", () => {
+    playground.innerHTML = "";
+    gameText.style.display = "none";
+    init()
+})
